3d38c2225f743501f71ef63f83fe0fc40e21e0d3
Ryan B <bombingrun3@gmail.com>
2025-05-01 00:40:26 -0400

    Styled Projects Cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,40 +1,51 @@
 import "./Projects.css";
+import EcoLink from "../../assets/Project-Screenshots/EcoLink-main.png";
 
 function Projects() {
   return (
-    <section className="projects">
+    <section className="projects app__section">
       <h2 className="projects__title">Recent Projects</h2>
       <ul className="projects__gallery">
         <li className="projects__card">
           <img
             className="projects__card-image"
             alt="My Growth Journal"
-            src="x"
+            src={EcoLink}
           ></img>
-          <h3 className="projects__card-title">My Growth Journal</h3>
-          <p className="projects__card-description">
-            A journaling app that presents prompts and allows users to write and
-            save entries locally. Built for personal growth and self-reflection,
-            with a minimal and focused interface.
-          </p>
+          <div className="projects__card-info">
+            <h3 className="projects__card-title">My Growth Journal</h3>
+            <p className="projects__card-description">
+              A journaling app that presents prompts and allows users to write
+              and save entries locally. Built for personal growth and
+              self-reflection, with a minimal and focused interface.
+            </p>
+          </div>
         </li>
         <li className="projects__card">
-          <img className="projects__card-image" alt="EcoLink" src="x"></img>
-          <h3 className="projects__card-title">EcoLink</h3>
-          <p className="projects__card-description">
-            A community-driven platform that connects users with eco-friendly
-            businesses and initiatives. Features include user submissions, map
-            integration, and sustainability tagging.
-          </p>
+          <img
+            className="projects__card-image"
+            alt="EcoLink"
+            src={EcoLink}
+          ></img>
+          <div className="projects__card-info">
+            <h3 className="projects__card-title">EcoLink</h3>
+            <p className="projects__card-description">
+              A community-driven platform that connects users with eco-friendly
+              businesses and initiatives. Features include user submissions, map
+              integration, and sustainability tagging.
+            </p>
+          </div>
         </li>
         <li className="projects__card">
-          <img className="projects__card-image" alt="Spots" src="x"></img>
-          <h3 className="projects__card-title">Spots</h3>
-          <p className="projects__card-description">
-            A social media-style app where users can create posts with images
-            and captions, like posts, and delete their own. Fully responsive and
-            styled for mobile-first usability.
-          </p>
+          <img className="projects__card-image" alt="Spots" src="x"></img>{" "}
+          <div className="projects__card-info">
+            <h3 className="projects__card-title">Spots</h3>
+            <p className="projects__card-description">
+              A social media-style app where users can create posts with images
+              and captions, like posts, and delete their own. Fully responsive
+              and styled for mobile-first usability.
+            </p>
+          </div>
         </li>
         <li className="projects__card">
           <img
@@ -42,12 +53,14 @@ function Projects() {
             alt="What To Wear App"
             src="x"
           ></img>
-          <h3 className="projects__card-title">WTWR</h3>
-          <p className="projects__card-description">
-            A weather-based clothing recommendation app built in React. It uses
-            a weather API to suggest clothing based on current temperature
-            conditions, promoting smart and comfortable dressing.
-          </p>
+          <div className="projects__card-info">
+            <h3 className="projects__card-title">WTWR</h3>
+            <p className="projects__card-description">
+              A weather-based clothing recommendation app built in React. It
+              uses a weather API to suggest clothing based on current
+              temperature conditions, promoting smart and comfortable dressing.
+            </p>
+          </div>
         </li>
       </ul>
     </section>
