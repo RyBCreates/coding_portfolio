af2c815c364edd0167b6ec9161b8b96b06bd2ab7
Ryan B <bombingrun3@gmail.com>
2025-04-30 23:51:58 -0400

    Minor adjustments to Header/Navbar styling, and added content to Projects Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,6 @@ function Home() {
       <div className="home__text">
         <h2 className="home__intro">My Name is:</h2>
         <p className="home__name">Ryan Bommarito</p>
-        {/* <h2 className="home__name">
-          <span className="home__intro">My name is: </span>Ryan
-        </h2> */}
         <CycleText />
         <button className="home__contact-button">Hire Me!</button>
       </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,20 +3,53 @@ import "./Projects.css";
 function Projects() {
   return (
     <section className="projects">
-      <h2>
-        📝 My Growth Journal A journaling app that presents prompts and allows
-        users to write and save entries locally. Built for personal growth and
-        self-reflection, with a minimal and focused interface. 🌱 EcoLink A
-        community-driven platform that connects users with eco-friendly
-        businesses and initiatives. Features include user submissions, map
-        integration, and sustainability tagging. 📸 Spots A social media-style
-        app where users can create posts with images and captions, like posts,
-        and delete their own. Fully responsive and styled for mobile-first
-        usability. 🌦 WTWR (What to Wear Right) A weather-based clothing
-        recommendation app built in React. It uses a weather API to suggest
-        clothing based on current temperature conditions, promoting smart and
-        comfortable dressing.
-      </h2>
+      <h2 className="projects__title">Recent Projects</h2>
+      <ul className="projects__gallery">
+        <li className="projects__card">
+          <img
+            className="projects__card-image"
+            alt="My Growth Journal"
+            src="x"
+          ></img>
+          <h3 className="projects__card-title">My Growth Journal</h3>
+          <p className="projects__card-description">
+            A journaling app that presents prompts and allows users to write and
+            save entries locally. Built for personal growth and self-reflection,
+            with a minimal and focused interface.
+          </p>
+        </li>
+        <li className="projects__card">
+          <img className="projects__card-image" alt="EcoLink" src="x"></img>
+          <h3 className="projects__card-title">EcoLink</h3>
+          <p className="projects__card-description">
+            A community-driven platform that connects users with eco-friendly
+            businesses and initiatives. Features include user submissions, map
+            integration, and sustainability tagging.
+          </p>
+        </li>
+        <li className="projects__card">
+          <img className="projects__card-image" alt="Spots" src="x"></img>
+          <h3 className="projects__card-title">Spots</h3>
+          <p className="projects__card-description">
+            A social media-style app where users can create posts with images
+            and captions, like posts, and delete their own. Fully responsive and
+            styled for mobile-first usability.
+          </p>
+        </li>
+        <li className="projects__card">
+          <img
+            className="projects__card-image"
+            alt="What To Wear App"
+            src="x"
+          ></img>
+          <h3 className="projects__card-title">WTWR</h3>
+          <p className="projects__card-description">
+            A weather-based clothing recommendation app built in React. It uses
+            a weather API to suggest clothing based on current temperature
+            conditions, promoting smart and comfortable dressing.
+          </p>
+        </li>
+      </ul>
     </section>
   );
 }
