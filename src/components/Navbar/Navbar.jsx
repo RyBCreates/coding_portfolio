63d4baae7f6a29be3ccb5b753d0b9337806584b2
Ryan B <bombingrun3@gmail.com>
2025-07-14 13:58:53 -0500

    Fixed Styling issues on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,28 +10,6 @@ function Navbar() {
   const handleToggle = () => setMenuOpen(!menuOpen);
   const handleClose = () => setMenuOpen(false);
   return (
-    // <nav className="navbar">
-    //   <ul className="navbar__links">
-    //     <NavLink to="/" className="navbar__link-home">
-    //       <li className="navbar__link">Home</li>
-    //     </NavLink>
-    //     <NavLink to="/about" className="navbar__link-about">
-    //       <li className="navbar__link">About</li>
-    //     </NavLink>
-
-    //     <NavLink to="/skills" className="navbar__link-skills">
-    //       <li className="navbar__link">Skills</li>{" "}
-    //     </NavLink>
-
-    //     <NavLink to="/projects" className="navbar__link-projects">
-    //       <li className="navbar__link">Projects</li>
-    //     </NavLink>
-
-    //     <NavLink to="/contact" className="navbar__link-contact">
-    //       <li className="navbar__link">Contact</li>
-    //     </NavLink>
-    //   </ul>
-    // </nav>
     <nav className="navbar">
       <button
         className={`navbar__hamburger ${menuOpen ? "open" : ""}`}
@@ -76,5 +54,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-/* Fix the Hamburger Menu Functionality */
